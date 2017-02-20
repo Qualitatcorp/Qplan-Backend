f75ae4249e8694f05f95d7e5ed57fcbd0d1d1919
Ruben <rbntejeda@hotmail.com>
2017-02-20 11:37:37 -0300

    Init

---
+++ config/db.example.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-blog',
+    'username' => 'root',
+    'password' => '164352',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,   
+];
