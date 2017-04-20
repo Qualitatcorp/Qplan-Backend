e21441d3ca194e6f8c6090d35eae4f3d00a26b7f
Ruben <rbntejeda@hotmail.com>
2017-04-20 09:58:31 -0300

    Actualiza Informacion tabla orden de trabjo

--- modules/v1/models/OrdenTrabajo.old.php
+++ modules/v1/models/OrdenTrabajo.old.php
@@ -12,8 +12,6 @@ use Yii;
  * @property integer $esp_id
  * @property string $per_id
  * @property string $sol_id
- * @property string $personal
- * @property string $tipo
  * @property string $inicio
  * @property string $termino
  * @property string $direccion
@@ -38,13 +36,11 @@ class OrdenTrabajo extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['emp_id', 'esp_id', 'per_id', 'sol_id', 'inicio', 'termino', 'direccion','personal','tipo'], 'required'],
+            [['emp_id', 'esp_id', 'per_id', 'sol_id', 'inicio', 'termino', 'direccion','personal'], 'required'],
             [['emp_id', 'esp_id', 'per_id', 'sol_id'], 'integer'],
-            [['personal', 'tipo', 'direccion', 'estado'], 'string'],
-            ['personal', 'in', 'range' => ['ESTABLE', 'TRANSITORIO']],
-            ['tipo', 'in', 'range' => ['CERTIFICACIÓN', 'PRE-CERTIFICACIÓN']],
-            // ['estado', 'in', 'range' => ['CERTIFICACIÓN', 'PRE-CERTIFICACIÓN']],
             [['inicio', 'termino'], 'safe'],
+            // [['personal', 'in', 'range' => ['ESTABLE', 'TRANSITORIO']]],
+            [['direccion', 'estado'], 'string'],
             [['sol_id'], 'exist', 'skipOnError' => true, 'targetClass' => OrdenTrabajoSolicitud::className(), 'targetAttribute' => ['sol_id' => 'id']],
             [['emp_id'], 'exist', 'skipOnError' => true, 'targetClass' => Empresa::className(), 'targetAttribute' => ['emp_id' => 'id']],
             [['esp_id'], 'exist', 'skipOnError' => true, 'targetClass' => Especialidad::className(), 'targetAttribute' => ['esp_id' => 'id']],
@@ -56,16 +52,15 @@ class OrdenTrabajo extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => 'Orden de Trabajo',
-            'emp_id' => 'Empresa Contratista',
-            'esp_id' => 'Especialidad',
-            'per_id' => 'Perfil de evaluación',
-            'sol_id' => 'Solicitud',
-            'personal' => 'Tipo de Personal',
-            'tipo' => 'Tipo de Certificación',
-            'inicio' => 'Fecha de inicio',
-            'termino' => 'Fecha de termino',
-            'direccion' => 'Dirección de servicio',
+            'id' => 'ID',
+            'emp_id' => 'Emp ID',
+            'esp_id' => 'Esp ID',
+            'per_id' => 'Per ID',
+            'sol_id' => 'Sol ID',
+            'personal'=>'Tipo de Personal',
+            'inicio' => 'Inicio',
+            'termino' => 'Termino',
+            'direccion' => 'Direccion',
             'estado' => 'Estado',
         ];
     }
@@ -119,48 +114,11 @@ class OrdenTrabajo extends \yii\db\ActiveRecord
         return $this->hasOne(Perfil::className(), ['id' => 'per_id']);
     }
 
-    // public function extraFields()
-    // {
-    //     return ['trabajador','mandante','fichas','solicitud','empresa','especialidad','perfil','countfichas','usuario'];
-    // }
-
-    // public function getFichas()
-    // {
-    //     return $this->hasMany(Ficha::className(), ['ot_id' => 'id']);
-    // }
-
-    // public function getSol()
-    // {
-    //     return $this->hasOne(OrdenTrabajoSolicitud::className(), ['id' => 'sol_id']);
-    // }
-
-    // public function getEmp()
-    // {
-    //     return $this->hasOne(Empresa::className(), ['id' => 'emp_id']);
-    // }
-
-    // public function getEsp()
-    // {
-    //     return $this->hasOne(Especialidad::className(), ['id' => 'esp_id']);
-    // }
-
-    // public function getSol0()
-    // {
-    //     return $this->hasOne(OrdenTrabajoSolicitud::className(), ['id' => 'sol_id']);
-    // }
-
-    // public function getPer()
-    // {
-    //     return $this->hasOne(Perfil::className(), ['id' => 'per_id']);
-    // }
 
     // public function getOrdenTrabajoTrabajadors()
     // {
     //     return $this->hasMany(OrdenTrabajoTrabajador::className(), ['ot_id' => 'id']);
     // }
 
-    // public function getTras()
-    // {
-    //     return $this->hasMany(Trabajador::className(), ['id' => 'tra_id'])->viaTable('orden_trabajo_trabajador', ['ot_id' => 'id']);
-    // }
+
 }
